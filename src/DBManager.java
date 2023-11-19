21c2375ad5bc48b57447387f97a63c40b70cb2e2
Michael <Shires>
2023-11-19 12:15:35 -0500

    Added all the files from 1.0, dealt with some intelliJ shenanigans but this is cleaner now.

--- src/DBManager.java
+++ src/DBManager.java
@@ -3,13 +3,24 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBManager {
-    public static void main(String[] args) {
+
+    boolean registerUser(String userID, byte[] passcode)
+    {
+        return true;
+    }
+
+    boolean verifyCredentials(String userID, byte[] passcode)
+    {
+        return true;
+    }
+
+    private Connection connectToDB()
+    {
         String url = "jdbc:mysql://localhost:3306/credentialbase";
         String username = "java";
         String password = "password";
 
         System.out.println("Connecting database ...");
-
         try (Connection connection = DriverManager.getConnection(url, username, password)) {
             System.out.println("Database connected!");
         } catch (SQLException e) {
