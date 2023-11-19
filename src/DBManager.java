c6686e7eb9ffc73ab2b36844ba474a8f501d318e
Michael <Shires>
2023-11-19 13:20:53 -0500

    Was able to insert a user into the database, and query and verify credentials! Next is getting encryption/decryption cleared up, then parse input, and then incorporate into mobile app.

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,31 +1,60 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.Arrays;
 
 public class DBManager {
 
-    boolean registerUser(String userID, byte[] passcode)
-    {
-        return true;
+    static void registerUser(String userID, byte[] passcode) throws IllegalStateException {
+        String update = "INSERT INTO credentials(USERID, PASSCODE) VALUES ('" + userID + "', '" +
+                Arrays.toString(passcode) + "');";
+        try(Statement stmt = connectToDB().createStatement())
+        {
+            stmt.executeUpdate(update);
+        }
+        catch(SQLException e) {
+            throw new IllegalStateException((e));
+        }
     }
 
-    boolean verifyCredentials(String userID, byte[] passcode)
-    {
-        return true;
+    static boolean verifyCredentials(String userID, byte[] passcode) throws IllegalStateException {
+        String query = "SELECT PASSCODE FROM credentials WHERE USERID ='" + userID + "';";
+        try(Statement stmt = connectToDB().createStatement())
+        {
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            String actualCode = rs.getString("PASSCODE");
+            return Arrays.toString(passcode).equals(actualCode);
+        }
+        catch(SQLException e) {
+            throw new IllegalStateException((e));
+        }
     }
 
-    private Connection connectToDB()
+    private static Connection connectToDB() throws SQLException
     {
         String url = "jdbc:mysql://localhost:3306/credentialbase";
         String username = "java";
         String password = "password";
-
         System.out.println("Connecting database ...");
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
-            System.out.println("Database connected!");
-        } catch (SQLException e) {
-            throw new IllegalStateException("Cannot connect the database!", e);
-        }
+        Connection connection = DriverManager.getConnection(url, username, password);
+        System.out.println("Database connected!");
+        return connection;
     }
 
+    public static void main(String[] args) throws IllegalStateException {
+        String testUser = "ted";
+        byte[] testData = new byte[8];
+        for(int i = 0; i < 8; i++)
+        {
+            testData[i] =3;
+        }
+        registerUser(testUser, testData);
+        if(verifyCredentials(testUser, testData))
+        {
+            System.out.println("It worked!");
+        }
+        else
+        {
+            System.out.println("It didn't work :(");
+        }
+    }
 }
